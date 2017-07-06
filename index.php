7d3822f990f2d2669883a4054882899877e0ed52
shazzner <shazzner@heavyheavy.com>
2017-07-06 18:37:12 +0000

    Typesetting & padding fixes for category boxes

--- index.php
+++ index.php
@@ -12,23 +12,24 @@ get_header(); viability_blog_hero(); ?>
 <section class="insight-section">
     <div class="insight-container">
         <?php printf ( '<h1>%1$s</h1>',
-        __( get_bloginfo( 'description' ), 'insight' ) );
+                       __( get_bloginfo( 'description' ), 'insight' ) );
         insight_index_search(); ?>
     </div>
 </section>
 <section class="insight-section">
     <div class="insight-container">
         <div class="category-container">
-            <?php  foreach ( get_categories( array(
+            <?php foreach ( get_categories( array(
                 'orderby' => 'name',
-                'order' => 'ASC', ) ) as $category ) {
+                'order' => 'ASC',
+                'number' => 2 ) ) as $category ) {
             ?>
                 <div class="category-container-box default-bg-color">
-            <?php printf ( '<h2>%1$s</h2><p>%2$s</p>',
+            <?php printf ( '<h2>%1$s</h2><h3>%2$s</h3>',
                            __( $category->name, 'insight' ),
                            __( $category->description, 'insight' ) ); ?>
                     <ul>
-                <?php foreach ( get_posts ( array(
+                <?php foreach ( get_posts ( array(                    
                     'posts_per_page' => 5,
                     'category' => array( $category->term_id ) ) ) as $post ) {
                     printf ( '<li><a href="%1$s">%2$s</a></li>',
@@ -36,11 +37,40 @@ get_header(); viability_blog_hero(); ?>
                              __( $post->post_title, 'insight' ) );
                 } ?>
                     </ul>
-                    <?php printf( '<a href="%1$s">%2$s</a>',
+                    <?php printf( '<div class="category-box-link"><a href="%1$s" class="button">%2$s</a></div>',
                                   get_category_link( $category->term_id ),
                                   __( 'See All', 'insight' )
                     ); ?>
-                </div>        
+                </div>
+            <?php 
+            }
+            ?>
+        </div>
+        <div class="category-container">
+            <?php foreach ( get_categories( array(
+                'orderby' => 'name',
+                'order' => 'ASC',
+                'offset' => 2,
+                'number' => 2 ) ) as $category ) {
+            ?>
+                <div class="category-container-box default-bg-color">
+            <?php printf ( '<h2>%1$s</h2><h3>%2$s</h3>',
+                           __( $category->name, 'insight' ),
+                           __( $category->description, 'insight' ) ); ?>
+                    <ul>
+                <?php foreach ( get_posts ( array(                    
+                    'posts_per_page' => 5,
+                    'category' => array( $category->term_id ) ) ) as $post ) {
+                    printf ( '<li><a href="%1$s">%2$s</a></li>',
+                             get_permalink(),
+                             __( $post->post_title, 'insight' ) );
+                } ?>
+                    </ul>
+                    <?php printf( '<div class="category-box-link"><a href="%1$s" class="button">%2$s</a></div>',
+                                  get_category_link( $category->term_id ),
+                                  __( 'See All', 'insight' )
+                    ); ?>
+                </div>
             <?php 
             }
             ?>
